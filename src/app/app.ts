2110b66516739ce15ff8cb9de402a6826a27c912
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2025-08-02 23:43:23 +0300

    Added header animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Header} from './shared/components/header/header';
-import {Footer} from './shared/components/footer/footer';
+import { Header } from './shared/components';
+import { Footer } from './shared/components';
 
 @Component({
   selector: 'app-root',
