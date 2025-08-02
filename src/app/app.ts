25f6553426c4e36e58c75ab375a5d55ed43c7b5c
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2025-08-03 01:05:30 +0300

    Added FontAwesome in a project

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components';
 import { Footer } from './shared/components';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [
+    RouterOutlet,
+    Header,
+    FontAwesomeModule,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
